a8fdda98f013effd1098ff4feead1c5c56ec7f96
VJ <vj1024@sina.com>
2026-03-30 17:35:32 +0800

    style: format build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 cd "$(dirname $0)"
 dir="$(pwd)"
 name=$(basename "$dir")
-tag=$(git describe --abbrev=0 --tags)
+tag=$(git describe --tags --exact-match 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
 gover=$(go env GOVERSION)
 
 set -e
@@ -11,28 +11,28 @@ set -e
 go mod tidy
 
 function build() {
-    os=${1:-$(go env GOOS)}
-    arch=${2:-$(go env GOARCH)}
-    compress=${3}
+  os=${1:-$(go env GOOS)}
+  arch=${2:-$(go env GOARCH)}
+  compress=${3}
 
-    version="${tag}-${gover}-${os}-${arch}"
-    echo "build $version"
-    GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -ldflags "-X 'main.Version=$version'" -o "$name"
+  version="${tag}-${gover}-${os}-${arch}"
+  echo "build $version"
+  GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -ldflags "-X 'main.Version=$version'" -o "$name"
 
-    if [[ "$compress" == "true" ]]; then
-        mkdir -p compress
-        tar -czf "compress/${name}-${version}.tar.gz" "${name}"
-        rm "$name"
-    fi
+  if [[ "$compress" == "true" ]]; then
+    mkdir -p compress
+    tar -czf "compress/${name}-${version}.tar.gz" "${name}"
+    rm "$name"
+  fi
 }
 
 if [[ "$1" == "release" ]]; then
-    build linux amd64 true
-    build linux arm64 true
-    build linux arm true
+  build linux amd64 true
+  build linux arm64 true
+  build linux arm true
 
-    build darwin amd64 true
-    build darwin arm64 true
+  build darwin amd64 true
+  build darwin arm64 true
 else
-    build
+  build
 fi
